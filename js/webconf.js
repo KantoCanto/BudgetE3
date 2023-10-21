fc336e5b45b5b39ae39fe05f757930ad9132744f
KantoCanto <bruno.canto.neves@gmail.com>
2023-10-21 17:01:17 +0100

    Speaker session with image click and swal

--- js/webconf.js
+++ js/webconf.js
@@ -2,6 +2,8 @@ window.onload = function () {
   //codigo para manipiçar o DOM
   const url_base = "https://budgetE3.herokuapp.com";
   const btnRegister = document.getElementById("btnRegister");
+
+  //Reggister Participants
   btnRegister.addEventListener("click", function () {
     swal({
       title: "Budget E3 SignUp",
@@ -46,4 +48,75 @@ window.onload = function () {
       }
     });
   });
+
+  //Get speakers from server
+  (async () => {
+    const renderSpeakers = document.getElementById("renderSpeakers");
+    let txtSpeakers = "";
+    const response = await fetch(`${urlBase}/conferences/1/speakers`);
+    const speakers = await response.json();
+
+    for (const speaker of speakers) {
+      let socialLinks = ""; // Store social media links
+
+      if (speaker.twitter !== null) {
+        socialLinks += `
+                <li class="list-inline-item">
+                    <a href="${speaker.twitter}" target="_blank">
+                        <i class="fab fa-twitter"></i>
+                    </a>
+                </li>`;
+      }
+      if (speaker.facebook !== null) {
+        socialLinks += `
+                <li class="list-inline-item">
+                    <a href="${speaker.facebook}" target="_blank">
+                        <i class="fab fa-facebook-f"></i>
+                    </a>
+                </li>`;
+      }
+      if (speaker.linkedin !== null) {
+        socialLinks += `
+                <li class="list-inline-item">
+                    <a href="${speaker.linkedin}" target="_blank">
+                        <i class="fab fa-linkedin-in"></i>
+                    </a>
+                </li>`;
+      }
+
+      txtSpeakers += `
+            <div class="col-sm-4">
+                <div class="team-member">
+                    <img class="mx-auto rounded-circle viewSpeaker" id="${speaker.idSpeaker}" src="${speaker.photo}" alt="">
+                    <h4>${speaker.name}</h4>
+                    <p class="text-muted">${speaker.role}</p>
+                    <ul class="list-inline social-buttons">
+                        ${socialLinks}
+                    </ul>
+                </div>
+            </div>`;
+
+      //manage image click
+      const btnView = document.getElementsByClassName("viewSpeaker");
+      for (let i = 0; i < btnView.length; i++) {
+        btnView[i].addEventListener("click", () => {
+          for (const speaker of speakers) {
+            if (speaker.idSpeaker == btnView[i].getAttribute("id")) {
+              //modal window
+              swal({
+                title: speaker.name,
+                text: speaker.bio,
+                imageUrl: speaker.photo,
+                imageWidth: 400,
+                imageHeight: 400,
+                imageAlt: "Speaker Photo",
+                animation: false,
+              });
+            }
+          }
+        });
+      }
+    }
+    renderSpeakers.innerHTML = txtSpeakers;
+  })();
 };
